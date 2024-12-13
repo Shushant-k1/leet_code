ef668a3fcaa33544fcbb6373da9777283492dfef
shushant kumar <rekhaubgb@gmail.com>
2024-12-13 22:39:44 +0530

    Time: 2 ms (91.32%) | Memory: 17.1 MB (27.56%) - LeetSync

---
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.py
@@ -0,0 +1,11 @@
+class Solution:
+    def pickGifts(self, gifts: List[int], k: int) -> int:
+        ans = 0
+        nums = [-i for i in gifts]
+        heapq.heapify(nums)
+        for i in range(k) :
+            x = -1 * heapq.heappop(nums)
+            x = int(math.sqrt(x))
+            heapq.heappush(nums , -1 * x)
+
+        return -1 * sum(nums)
