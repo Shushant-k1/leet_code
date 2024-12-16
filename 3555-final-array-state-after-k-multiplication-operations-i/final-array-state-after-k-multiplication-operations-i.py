fcd2cdb5cd63ab8490d7d9c449f7a83f5077826e
shushant kumar <rekhaubgb@gmail.com>
2024-12-16 21:42:43 +0530

    Time: 7 ms (13.13%) | Memory: 17.6 MB (11.77%) - LeetSync

--- 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.py
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.py
@@ -1,14 +1,27 @@
 class Solution:
     def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
-        _heap = [(nums[i] , i)for i in range(len(nums))]
-        heapq.heapify(_heap)
 
-        for i in range(k) :
-            ele  , ind = heapq.heappop(_heap)
-            heapq.heappush(_heap , (ele * multiplier , ind))
+        mod = 10 ** 9 + 7
+        if multiplier == 1 :
+            return nums
         
-
-        for i in _heap :
-            ele , ind = i
+        m = max(nums)
+        n = len(nums)
+        pq = [[nums[i] , i] for i in range(n)]
+        heapify(pq)
+        while k and pq[0][0] * multiplier <= m :
+            ele ,ind = heappop(pq)
+            heappush(pq , [ele * multiplier , ind])
+            k -= 1
+        
+        pq.sort()
+        q , k = divmod(k , n)
+        for i in range(n) :
+            pq[i][0] = pq[i][0] * pow(multiplier , q , mod) % mod
+        for i in range(k) :
+            pq[i][0] = pq[i][0] * multiplier % mod
+        
+        for i in pq :
+            ele , ind  = i
             nums[ind] = ele
-        return nums
+        return nums 
