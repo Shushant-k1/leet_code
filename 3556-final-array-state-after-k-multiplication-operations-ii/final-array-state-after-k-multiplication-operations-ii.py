9f26e71ed6101da2fed379e3477319b5f9fd1f9d
shushant kumar <rekhaubgb@gmail.com>
2024-12-16 21:40:53 +0530

    Time: 339 ms (41.02%) | Memory: 20.4 MB (12.68%) - LeetSync

---
+++ 3556-final-array-state-after-k-multiplication-operations-ii/final-array-state-after-k-multiplication-operations-ii.py
@@ -0,0 +1,25 @@
+class Solution:
+    def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
+        mod = 10 ** 9 + 7
+        if multiplier == 1 :
+            return nums
+        m = max(nums)
+        n = len(nums)
+        pq = [[nums[i] , i] for i in range(n)]
+        heapify(pq)
+        while k and pq[0][0] * multiplier <= m :
+            ele ,ind = heappop(pq)
+            heappush(pq , [ele * multiplier , ind])
+            k -= 1
+        
+        pq.sort()
+        q , k = divmod(k , n)
+        for i in range(n) :
+            pq[i][0] = pq[i][0] * pow(multiplier , q , mod) % mod
+        for i in range(k) :
+            pq[i][0] = pq[i][0] * multiplier % mod
+        
+        for i in pq :
+            ele , ind  = i
+            nums[ind] = ele
+        return nums 
