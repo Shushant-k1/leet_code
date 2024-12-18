081ae934ff67ba513317e7183ef6d343b566949c
shushant kumar <rekhaubgb@gmail.com>
2024-12-18 22:49:43 +0530

    Time: 3 ms (55.13%) | Memory: 18 MB (10.22%) - LeetSync

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
@@ -0,0 +1,15 @@
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        next_small = [prices[-1]]
+        n = len(prices)
+        st = [prices[-1]]
+        for i in range( n - 2 , -1 , -1 ) :
+            if st[-1] > prices[i] :
+                while st and st[-1] > prices[i] :
+                    st.pop() 
+            if st :
+                next_small.append(prices[i] - st[-1])
+            else :
+                next_small.append(prices[i])
+            st.append(prices[i])
+        return next_small[::-1]
