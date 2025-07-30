e7b27b988b6f7e22be0485c2e4093c20de4f4104
shushant kumar <rekhaubgb@gmail.com>
2025-07-31 05:07:18 +0530

    Time: 125 ms (68.72%) | Memory: 19.4 MB (93.33%) - LeetSync

---
+++ 542-01-matrix/01-matrix.py
@@ -0,0 +1,28 @@
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        m , n = len(mat) , len(mat[0])
+        if not mat :
+            return []
+        
+        q = deque()
+        max_val = m * n
+
+        for i in range(m ) :
+            for j in range(n) :
+                if mat[i][j] == 0 :
+                    q.append((i  , j))
+                else :
+                    mat[i][j] = max_val
+        
+        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+
+        while q :
+            row , col = q.popleft()
+            for dr , dc in dirs :
+                r , c = row + dr , col + dc
+                if 0 <= r < m and 0 <= c < n and mat[r][c] > mat[row][col] + 1:
+                    q.append((r , c))
+                    mat[r][c] = mat[row][col] + 1
+        
+        return mat
