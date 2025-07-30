48ef56d0a3094b967385017ae4e4c65a04387585
shushant kumar <rekhaubgb@gmail.com>
2025-07-30 18:59:57 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (93.06%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,29 +1,31 @@
+from collections import deque
+from typing import List
+
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        n , m = len(grid) , len(grid[0])
-
-        rotten =  []
-        cnt = 0
-        for i in range(n) :
-            for j in range(m) :
-                if grid[i][j] == 2 :
-                    rotten.append((i ,j))
-                elif grid[i][j] ==1 :
-                    cnt += 1
+        n, m = len(grid), len(grid[0])
+        rotten = deque()
+        fresh_count = 0
         
-        vis = set()
-        dirs = [(0 , -1) , (0 , 1) , (1 , 0) , (-1 , 0)]
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 2:
+                    rotten.append((i, j))
+                elif grid[i][j] == 1:
+                    fresh_count += 1
+        
+        dirs = [(0, -1), (0, 1), (1, 0), (-1, 0)]
         time = 0
-        while rotten and cnt :
-            time += 1
-            for i in range(len(rotten)) :
-                dx , dy = rotten.pop(0)
-                for x , y in dirs :
-                    new_x , new_y = x + dx , y + dy
-                    if 0 <= new_x < n and 0<= new_y < m and grid[new_x][new_y] == 1 and (new_x , new_y) not in vis :
-                        vis.add((new_x , new_y))
-                        rotten.append((new_x, new_y))
-                        cnt -= 1
-        return -1 if cnt > 0 else time
-
         
+        while rotten and fresh_count:
+            for _ in range(len(rotten)):
+                x, y = rotten.popleft()
+                for dx, dy in dirs:
+                    nx, ny = x + dx, y + dy
+                    if 0 <= nx < n and 0 <= ny < m and grid[nx][ny] == 1:
+                        grid[nx][ny] = 2  # mark as rotten
+                        rotten.append((nx, ny))
+                        fresh_count -= 1
+            time += 1
+        
+        return -1 if fresh_count > 0 else time
