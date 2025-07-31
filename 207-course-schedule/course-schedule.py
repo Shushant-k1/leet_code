cce3369519ded726a20915ff8d49d00defdbbd6e
shushant kumar <rekhaubgb@gmail.com>
2025-07-31 17:54:42 +0530

    Time: 11 ms (13.82%) | Memory: 19.3 MB (41.27%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,57 +1,36 @@
 class Solution:
-
-    def adj(self , equations):
-        d = {}
-        for node in (equations):
-            en , st = node
-            
-            if st not in d:
-                d[st] = []
-            d[st].append((en))
-            
-            # if en not in d :
-            #     d[en] = []
-            # d[en].append(st)
-        return d
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        adj_mat = self.adj(prerequisites)
-        indegree = [0] * numCourses 
-        for i , j  in prerequisites :
-            indegree[i] += 1
-        ind_zer_ele = deque()
-        print(indegree)
-        for i in range(numCourses)  :
-            if indegree[i] == 0 : ind_zer_ele.append(i)
-        if len(ind_zer_ele) == 0 : return False
-        print(adj_mat)
-        print(ind_zer_ele)
-        while ind_zer_ele :
-            ele = ind_zer_ele.popleft()
-            if ele not in adj_mat :
-                continue
-            for neigh in adj_mat[ele] :
-                indegree[neigh] -= 1
-                if indegree[neigh] == 0 :
-                    ind_zer_ele.append(neigh)
-        print(indegree)
-        for i in indegree :
-            if i != 0 : return False
+        adj = self.adj_mat(prerequisites , numCourses)
+        print(adj)
+        vis = set()
+        restack = set()
+        cnt = 0
+        for i in range(numCourses) :
+            if i  not in vis :
+                if  self.dfs(adj , vis ,i, restack) :
+                    return False
         return True
-                
-        # Perform BFS-based topological sort
-        # visited_count = 0
-        # while ind_zer_ele:
-        #     ele = ind_zer_ele.popleft()
-        #     visited_count += 1  # Count how many nodes we can visit
-            
-        #     if ele not in adj_mat:
-        #         continue
-        #     for neigh in adj_mat[ele]:
-        #         indegree[neigh] -= 1
-        #         if indegree[neigh] == 0:
-        #             ind_zer_ele.append(neigh)
-        
-        # # If we visited all courses, return True; otherwise, there's a cycle
-        # return visited_count == numCourses
+    
 
+    def dfs(self ,adj , vis , cur , restack)   :
+        vis.add(cur) 
+        restack.add(cur)
+
+        for neigh in adj[cur] :
+            if neigh not in vis :
+                if self.dfs(adj , vis , neigh  , restack ) : 
+                    return True
+            elif neigh in restack :
+                return True
+        restack.remove(cur)
+        return False
+    
+    def adj_mat(self , prerequisites, numCourses) :
+
+        adj = [[] for i in range(numCourses)]
+
+        for i , j in (prerequisites) :
+            adj[j].append(i)
+
+        return adj
 
