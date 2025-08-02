2ee17d697dfc06350ac05132f7f2e52f0f7d60f4
shushant kumar <rekhaubgb@gmail.com>
2025-08-02 17:27:30 +0530

    Time: 603 ms (27.23%) | Memory: 18.3 MB (75.69%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.py
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.py
@@ -1,33 +1,29 @@
 class Solution:
     def removeStones(self, stones: List[List[int]]) -> int:
+        
         n = len(stones)
-        group = [-1] * n  # dp-like array: group[i] = component id of i-th stone
-        component_id = 0
-        
-        # Tabular adjacency: store rows and columns
-        row_map = {}
-        col_map = {}
-        
-        for i, (x, y) in enumerate(stones):
-            row_map.setdefault(x, []).append(i)
-            col_map.setdefault(y, []).append(i)
+        adj = [[] for i in range(n)]
 
-        for i in range(n):
-            if group[i] == -1:
-                # simulate component marking without DFS/Union-Find
-                stack = [i]
-                while stack:
-                    u = stack.pop()
-                    if group[u] != -1:
-                        continue
-                    group[u] = component_id
-                    x, y = stones[u]
-
-                    for nei in row_map[x] + col_map[y]:
-                        if group[nei] == -1:
-                            stack.append(nei)
-
-                component_id += 1  # next component
+        for i in range(n ) :
+            for j in range(i + 1 , n) :
+                if stones[i][0] == stones[j][0] or stones[i][1] == stones[j][1] :
+                    adj[i].append(j)
+                    adj[j].append(i)
         
-        # Result: total stones - total components
-        return n - component_id
+        components = 0
+        vis = set()
+        for i in range(n) :
+            if i not in vis :
+                components += 1
+                self.dfs(vis , i , adj)
+        
+        return n - components
+
+    def dfs(self , vis , node , adj) :
+
+        vis.add(node)
+        for neigh in adj[node] :
+            if neigh not in vis:
+                vis.add(neigh)
+                self.dfs(vis , neigh , adj)
+    
