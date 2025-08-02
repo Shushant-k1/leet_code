9bc4f387297a23ef8d40e21f2724f05c2725a377
shushant kumar <rekhaubgb@gmail.com>
2025-08-02 15:39:16 +0530

    Time: 35 ms (78.64%) | Memory: 38.4 MB (24.74%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.py
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.py
@@ -1,26 +1,30 @@
 class Solution:
     def makeConnected(self, n: int, connections: List[List[int]]) -> int:
-        if len(connections) < n - 1:
-            return -1  # Not enough cables to connect all computers
         
-        parent = list(range(n))
+        if len(connections) < n-1 :
+            return  - 1 
 
-        def find(x):
-            if parent[x] != x:
-                parent[x] = find(parent[x])  # Path compression
-            return parent[x]
+        adj = [[] for i in range(n)]
 
-        def union(x, y):
-            rootX = find(x)
-            rootY = find(y)
-            if rootX != rootY:
-                parent[rootY] = rootX  # Merge components
+        for u , v in connections :
+            adj[u].append(v)
+            adj[v].append(u)
+        
+        vis = set()
 
-        for u, v in connections:
-            union(u, v)
-
-        # Count unique roots
-        components = sum(1 for i in range(n) if find(i) == i)
-
-        # We need (components - 1) operations to connect them
-        return components - 1
+        cnt = 0
+        for i in range(n) :
+            if i not in vis:
+                self.dfs(i , adj , -1 , vis)
+                cnt += 1
+        
+        return cnt - 1
+        
+        
+    def dfs(self , node , adj , par ,vis) :
+        
+        vis.add(node)
+        
+        for neigh in adj[node] :
+            if neigh not in vis :
+                self.dfs(neigh , adj ,node ,vis)
