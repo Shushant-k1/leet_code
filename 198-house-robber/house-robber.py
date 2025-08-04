fddc0fb8485d66db16ccba3e875d4d7a79e252fa
shushant kumar <rekhaubgb@gmail.com>
2025-08-04 17:43:14 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (54.13%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -3,7 +3,7 @@ class Solution:
         n = len(nums)
         if n == 1 : return nums[0]
 
-        dp = [float('-inf') for i in range(len(nums))]
+
 
         a,  b  = nums[0] , max(nums[0] , nums[1])
 
