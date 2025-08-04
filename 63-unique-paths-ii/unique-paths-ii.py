1de6451bb1df2d9c197bec61f4fe50de36dfa39b
shushant kumar <rekhaubgb@gmail.com>
2025-08-04 21:34:54 +0530

    Time: 1 ms (35.85%) | Memory: 18 MB (30.51%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -11,7 +11,7 @@ class Solution:
                 dp[i][0] = dp[i-1][0]
             else :
                 dp[i][0] = 0
-        print(dp)
+
         for i in range(1 ,m) :
             if grid[0][i] == 0 :
                 dp[0][i] = dp[0][i-1]
@@ -22,6 +22,5 @@ class Solution:
             for j in range(1 , m) :
                 if grid[i][j] == 0 :
                     dp[i][j] = dp[i-1][j] + dp[i][j -1 ]
-                
-        print(dp)
+
         return dp[n-1][m-1]
