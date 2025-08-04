5f552e745fce75a8ca22ecab09df4db596556ffa
shushant kumar <rekhaubgb@gmail.com>
2025-08-04 16:14:38 +0530

    Time: 35 ms (97.26%) | Memory: 26.7 MB (28.57%) - LeetSync

--- 403-frog-jump/frog-jump.py
+++ 403-frog-jump/frog-jump.py
@@ -5,23 +5,20 @@ class Solution:
         if stones[1] != 1 : return False
         d = set(stones)
         end = stones[-1]
-        dp = {}
-        return self.helper(d , 0 , 0, end ,dp)
 
-    
-    def helper(self , d , cur  , k  , end , dp) :
 
-        if cur == end : return True
-        if (cur , k) in dp : return dp[(cur , k )]
+        @cache
+        def helper( cur  , k  ) :
 
-        if cur not in d :  return False
+            if cur == end : return True
+            if cur not in d : return False
+            for i in (k - 1 , k + 1 , k) :
+                if i > 0 :
+                    if helper( cur + i, i , ) : 
+                        return True
+            return False
+        
+        return helper( 0 , 0 )
 
-        for i in (k - 1 , k + 1 , k) :
-            if i > 0 :
-                if self.helper(d , cur + i, i , end  , dp) : 
-                    dp[(cur , k)] = True
-                    return True
-        dp[(cur , k)] = False
-        return dp[(cur , k)]
 
         
