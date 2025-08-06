9bd4dc4be89e1878e76de6feb0df3f6b3ac1c726
shushant kumar <rekhaubgb@gmail.com>
2025-08-07 00:09:18 +0530

    Time: 2283 ms (5.01%) | Memory: 105.1 MB (7.25%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.py
+++ 368-largest-divisible-subset/largest-divisible-subset.py
@@ -8,17 +8,17 @@ class Solution:
         
     
 
-    def helper(self , nums ,idx , cur_max , dp) :
+    def helper(self , nums ,idx , cur_idx , dp) :
 
         if idx == len(nums) :
             return []
-        if (idx , cur_max) in dp : return dp[(idx , cur_max)]
+        if (idx , cur_idx) in dp : return dp[(idx , cur_idx)]
 
-        not_take = self.helper(nums , idx + 1 , cur_max , dp) 
+        not_take = self.helper(nums , idx + 1 , cur_idx , dp) 
         take = []
-        if cur_max == -1 or nums[idx] % cur_max == 0 :
-            take = self.helper(nums , idx + 1 , max(cur_max ,nums[idx] ) , dp)  + [nums[idx]]
+        if cur_idx == -1 or nums[idx] % nums[cur_idx] == 0 :
+            take = self.helper(nums , idx + 1 , idx , dp)  + [nums[idx]]
         
-        dp[(idx , cur_max)] = take if len(take) > len(not_take) else not_take
+        dp[(idx , cur_idx)] = take if len(take) > len(not_take) else not_take
 
-        return dp[(idx , cur_max)]
+        return dp[(idx , cur_idx)]
