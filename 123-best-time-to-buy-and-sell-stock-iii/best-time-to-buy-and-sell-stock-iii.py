704049cd5eddee70448e8691373a834b260768a0
shushant kumar <rekhaubgb@gmail.com>
2025-08-06 13:42:41 +0530

    Time: 2632 ms (5.02%) | Memory: 115.3 MB (11.41%) - LeetSync

---
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -0,0 +1,33 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        
+        return self.helper(prices , 0 , 0 ,0 ,{})
+    
+    
+
+    def helper(self , prices , cur_idx , buy , cnt , dp) :
+
+        if cur_idx == len(prices) or cnt == 2 :
+            return 0
+        
+        if (cur_idx , buy , cnt) in dp :
+            return dp[(cur_idx , buy , cnt)]
+        
+        take = 0
+        not_take = 0
+        if buy == 0 :
+            take = self.helper(prices , cur_idx + 1, 1 , cnt , dp ) + - prices[cur_idx]
+            not_take = self.helper(prices , cur_idx + 1 , 0 , cnt , dp )
+            dp[(cur_idx , buy , cnt)] = max(take , not_take)
+
+        else :
+            take = self.helper(prices , cur_idx + 1 , 0 , cnt + 1 , dp) + prices[cur_idx] 
+            not_take = self.helper(prices , cur_idx  + 1 , buy , cnt , dp )
+            dp[(cur_idx , buy , cnt)] = max(take , not_take)
+
+        dp[(cur_idx , buy , cnt)] = max(take , not_take)
+
+        return dp[(cur_idx , buy , cnt)]
+
+
