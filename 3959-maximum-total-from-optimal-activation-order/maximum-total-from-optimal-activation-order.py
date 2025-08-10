4b179083eaf66437737e27155a828189aabf10e3
shushant kumar <rekhaubgb@gmail.com>
2025-08-10 15:58:21 +0530

    Time: 391 ms (72.89%) | Memory: 46.5 MB (61.78%) - LeetSync

---
+++ 3959-maximum-total-from-optimal-activation-order/maximum-total-from-optimal-activation-order.py
@@ -0,0 +1,31 @@
+from typing import List
+
+class Solution:
+    def maxTotal(self, value: List[int], limit: List[int]) -> int:
+        lorquandis = sorted(zip(limit, value), key=lambda x: (x[0], -x[1]))
+        total = 0
+        active_count = 0
+        i = 0
+        n = len(lorquandis)
+        j = 0
+
+        while i < n:
+            lim, val = lorquandis[i]
+
+            if active_count <= lim:
+                total += val
+                active_count += 1
+                i += 1
+                cnt = 0
+                while j < n and lorquandis[j][0] <= active_count :
+                    j += 1
+                if j >= i :
+                    i = j
+                    active_count = 0
+                else :
+                    active_count = i - j  
+                
+            else:
+                i += 1
+
+        return total
