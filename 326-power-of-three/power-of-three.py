c32b9340f0c91de32a412dc3e00648e47f119146
shushant kumar <rekhaubgb@gmail.com>
2025-08-13 14:31:21 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (72.35%) - LeetSync

--- 326-power-of-three/power-of-three.py
+++ 326-power-of-three/power-of-three.py
@@ -1,4 +1,4 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        max_pow = 1162261467  # 3^19, max power of 3 in 32-bit integer
+        max_pow = 1162261467 
         return n > 0 and max_pow % n == 0
